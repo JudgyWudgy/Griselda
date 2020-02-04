1fd1228ccb9e647af826fd02e0b8d607abdb4f07
Brandon <btorrey99@comcast.net>
2020-02-04 18:58:27 -0500

    First Checkin
    "Griselda, real rap music, you don't have to put this in, I'm doing this for fun".

---
+++ GriseldaExample/GriseldaExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GriseldaExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GriseldaMember BennyTheButcher = new GriseldaMember("Benny The Butcher");
+            GriseldaMember WestsideGun = new GriseldaMember("Westside Gun");
+            GriseldaMember ConwayTheMachine = new GriseldaMember("Conway The Machine");
+        }
+    }
+}
